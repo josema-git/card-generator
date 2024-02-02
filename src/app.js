1383235fb8132332d8b3b238e4fd94a8b124ff45
Jose Manuel <joseman6575@gmail.com>
2024-02-02 18:55:34 -0500

    changes2

--- src/app.js
+++ src/app.js
@@ -27,3 +27,37 @@ onload = function() {
     });
   }
 };
+
+function changeCard() {
+  let suits = ["♠", "♣", "♥", "♦"];
+  let numSuit = Math.floor(Math.random() * 4);
+  let cardValues = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+  ];
+  document.querySelectorAll(".suit").forEach((element) => {
+    element.innerHTML = suits[numSuit];
+  });
+  document.querySelector(".center").innerHTML =
+    cardValues[Math.floor(Math.random() * 13)];
+  if (numSuit > 1) {
+    document.querySelectorAll(".suit").forEach((element) => {
+      element.style.color = "red";
+    });
+  } else {
+    document.querySelectorAll(".suit").forEach((element) => {
+      element.style.color = "black";
+    });
+  }
+}
